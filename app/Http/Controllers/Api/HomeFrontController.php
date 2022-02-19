28aed65e44f3974c469a6c254565aaed7de7bc09
Eslam-atia442 <56205474+Eslam-atia442@users.noreply.github.com>
2022-02-19 21:31:00 +0200

    Update HomeFrontController.php

--- app/Http/Controllers/Api/HomeFrontController.php
+++ app/Http/Controllers/Api/HomeFrontController.php
@@ -107,7 +107,7 @@ class HomeFrontController extends Controller
         if ($validator->fails()) {
             return msgdata($request, failed(), $validator->messages()->first(), (object)[]);
         }
-        $data['user_id'] = auth('api')->user()->id;
+        $data['user_id'] = '5';
         Inbox::create($data);
         return msgdata($request, success(), 'تم الارسال بنجاح', $data);
 
