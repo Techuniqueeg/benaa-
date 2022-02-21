f85c76ad0f2563ad7b9f458f3b157750b3dd119f
Eslam-atia442 <56205474+Eslam-atia442@users.noreply.github.com>
2022-02-21 12:13:17 +0200

    faceboook and whatsapp

--- app/Http/Requests/SettingRequest.php
+++ app/Http/Requests/SettingRequest.php
@@ -27,6 +27,7 @@ class SettingRequest extends FormRequest
             'logo' => 'required|mimes:jpeg,jpg,png',
             'site_name' => 'required|string|max:191',
             'phone' => 'required|max:191|min:11|regex:/^([0-9\s\-\+\(\)]*)$/',
+            'facebook' => 'nullable|string|max:191',
             'location' => 'nullable|string|max:191',
             'location_url' => 'nullable|url|max:191',
             'whatsapp' => 'nullable|url|max:191',
